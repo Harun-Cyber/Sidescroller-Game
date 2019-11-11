79acda65d0a9940641c70bc4b616e9bcede39df2
Unknown <unknown@example.com>
2019-11-11 11:17:04 +0100

    Heb de Canvas width en height aangepast om zo de achtergrond met de tiles groter te kunnen maken. Heb een class aangemaakt met meerdere methodes zodat ik met javascript de achtergrond en de platformen kan veranderen

--- public/js/main.js
+++ public/js/main.js
@@ -8,18 +8,54 @@ function loadImage(url){
     });
 }
 
+class SpriteSheet{
+    constructor(image, width, height){
+        this.image = image;
+        this.width = width;
+        this.height = height;
+        this.tiles = new Map();
+    }
+
+    define(name, x, y){
+        const buffer = document.createElement('canvas');
+        buffer.width = this.width;
+        buffer.height = this.height;
+        buffer.getContext('2d').drawImage(
+            this.image, 
+            x * this.width, 
+            y * this.height, 
+            this.width, this.height, 
+            0, 0, this.width, this.height);
+        this.tiles.set(name, buffer);
+    }
+
+    draw(name, context, x, y){
+        const buffer = this.tiles.get(name);
+        context.drawImage(buffer, x, y);
+    }
+
+    drawTile(name, context, x, y){
+        this.draw(name, context, x * this.width, y * this.height);
+    }
+}
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
-
-context.fillRect(0, 0, 50, 50);
 
 loadImage('images/tiles.png').then(image => {
     const sprites = new SpriteSheet(image, 16, 16);
     sprites.define('ground', 0, 0);
-    sprites.draw('ground', context, 45, 62);
-    
-    
-    context.drawImage(image, 
-        0, 0, 16, 16, 
-        0 ,0, 16, 16);
+    sprites.define('sky', 3, 23);
+
+    for(let x = 0; x < 200; ++x){
+        for(let y = 0; y < 100; ++y ){
+            sprites.drawTile('sky', context, x, y);        
+        }
+    }
+
+    for(let x = 0; x < 200; ++x){
+        for(let y = 55; y < 100; ++y ){
+            sprites.drawTile('ground', context, x, y);        
+        }
+    }
 });
