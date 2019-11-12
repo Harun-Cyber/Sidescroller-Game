3b3c8bcc317bfdcbf3255a83871d0c9609486247
Unknown <unknown@example.com>
2019-11-12 20:52:22 +0100

    Heb uitgevogeld hoe de karakter van links naar rechts beweegt. Alleen de kleur verspreid zich mee met de karakter.

--- main.js
+++ main.js
@@ -4,18 +4,57 @@ var canvas = document.getElementById("canvas"),
     width = 500,
     height = 200,
     player = {
-      x : width/2,
+      x : width / 2,
       y : height - 5,
       width : 5,
-      height : 5
-    };
+      height : 5,
+      speed : 3,
+      MovementX : 0,
+      MovementY : 0
+    },
+    keys = [];
  
 canvas.width = width;
 canvas.height = height;
  
 function update(){
+  // De toetsen
+  if (keys[38]){
+    // Up Arrow toets
+  }
+  if (keys[39]){
+    // Right Arrow toets
+    if(player.MovementX < player.speed){
+      player.MovementX++
+    }
+  }
+  if (keys[37]){
+    // Left Arrow toets
+    if (player.MovementX > -player.speed){
+      player.MovementX--
+    }
+  }
+
+  player.x += player.MovementX;
+  player.y += player.MovementY;
+
+  if(player.x >= width-player.width){
+    player.x = width-player.width;
+  }
+  else if(player.x <= 0){
+    player.x = 0;
+  }
+
   // De speler
   context.fillStyle = "red";
   context.fillRect(player.x, player.y, player.width, player.height);
 }
+
+document.body.addEventListener("keydown", function(e){
+  keys[e.keyCode] = true;
+});
+document.body.addEventListener("keyup", function(e){
+  keys[e.keyCode] = false;
+});
+
 intervalID = setInterval(update, 1000 / gameFPS);
