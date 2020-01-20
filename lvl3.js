1a892b2cb10e4b8f2ece4780fdc1398a4b28e444
Unknown <unknown@example.com>
2020-01-20 17:02:39 +0100

    CSS af. Er zit een bug als je de app opstart. Er is een socende delay.

---
+++ lvl3.js
@@ -0,0 +1,20 @@
+var hoursHTML = document.querySelector("[data-number-hours]");
+var minutesHTML = document.querySelector("[data-number-minutes]");
+var secondsHTML = document.querySelector("[data-number-seconds]");
+
+function clockTime(element, rotation){
+    element.style.setProperty("--rotation", rotation *360);
+}
+
+function update(){
+    var date = new Date();
+    var seconds = date.getSeconds() / 60;
+    var minutes = date.getMinutes() / 60;
+    var hours = date.getHours() / 60;
+
+    clockTime(hoursHTML, hours);
+    clockTime(minutesHTML, minutes);
+    clockTime(secondsHTML, seconds);
+}
+
+setInterval(update, 1000);
