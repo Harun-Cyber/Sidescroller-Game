722813320f77e01904a9b1fc24c6682336762c29
Unknown <unknown@example.com>
2020-01-21 21:51:08 +0100

    Functie dat je van beurt wisselt en dat je op lege vlakken kunt klikken is af. Functie om te bekijken wie er heeft gewonnen wordt nog aan gewerkt.

--- lvl4.js
+++ lvl4.js
@@ -1,14 +1,40 @@
 var signsData = document.querySelectorAll("[data-signs]");
+var game = document.getElementById("game");
 var turnO;
-signsData.forEach(signs => {
-    signs.addEventListener("click", clickSigns, {once: true})
-});
+
+firstTurn();
+
+function firstTurn(){
+    turnO = false;
+    signsData.forEach(signs => {
+        signs.addEventListener("click", clickerSigns, {once: true})
+    });
+    hoverSigns();
+}
 
 function clickerSigns(){
     var signs = event.target;
     var turnCurrent = turnO ? "o" : "x";
-
     placerSigns(signs, turnCurrent);
+    witchTurn();
+    hoverSigns();
 }
 
-function placerSigns()
+function hoverSigns(){
+    game.classList.remove("o");
+    game.classList.remove("x");
+    if(turnO){
+        game.classList.add("o");
+    }
+    else{
+        game.classList.add("x");
+    }
+}
+
+function placerSigns(signs, turnCurrent){
+    signs.classList.add(turnCurrent);
+}
+
+function witchTurn(){
+    turnO = !turnO;
+}
